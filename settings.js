977b0266680324b9d33ca16ae4d7d9160ca496a3
polgahakolapatai-afk <polgahakolapatai@gmail.com>
2025-09-22 13:26:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'DF8AiI7C#-ybRGVAUL7jqltTiTvJl4r3b3ykuDH6JWl-BfzqmB88' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
